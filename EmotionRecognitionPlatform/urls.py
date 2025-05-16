6a7d9e6253831f917ab4a7a84eeceffc3fa37f3a
TlIsD <965229800@qq.com>
2025-05-16 15:08:17 +0800

    优化了å前端展示速度

--- EmotionRecognitionPlatform/urls.py
+++ EmotionRecognitionPlatform/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('rec/', include('audio.urls')),
+    path('', include('audio.urls')),
 ]
--- audio/urls.py
+++ audio/urls.py
@@ -2,5 +2,5 @@ from django.urls import path, include
 from .views import upload_file
 
 urlpatterns = [
-    path('res/', upload_file),
+    path('', upload_file),
 ]
